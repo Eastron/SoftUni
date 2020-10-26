a60dc6ff5b87c81b88172af1503702debe132314
Pascal Angelov <pascal_angelov@abv.bg>
2020-10-26 20:27:24 +0200

    Add files via upload

---
+++ Iterators and Comparators/07. Stack/CustomStack.cs
@@ -0,0 +1,51 @@
+﻿namespace IteratorsAndComparators
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.Intrinsics.X86;
+    using System.Text;
+    public class CustomStack<T> : IEnumerable<T>
+    {
+
+        public List<T> Collections { get; set; }
+
+        public CustomStack()
+        {
+            this.Collections = new List<T>();
+        }
+
+        public void Push(params T[] elements)
+        {
+            for (int i = 1; i < elements.Length; i++)
+            {
+                this.Collections.Add(elements[i]);
+            }
+        }
+        public void Pop()
+        {
+            if (this.Collections.Count == 0)
+            {
+                Console.WriteLine("No elements");
+            }
+            else
+            {
+                this.Collections.RemoveAt(this.Collections.Count - 1);
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var element in this.Collections)
+            {
+                yield return element;
+
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators/07. Stack/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace IteratorsAndComparators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            CustomStack<string> collections = new CustomStack<string>();
+            while (input != "END")
+            {
+                string[] token = input.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                switch (token[0])
+                {
+                    case "Push":
+                        {
+                            collections.Push(token);
+                        }
+                        break;
+                    case "Pop":
+                        {
+                            collections.Pop();
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            collections.Collections.Reverse();
+            foreach (var element in collections.Collections)
+            {
+                Console.WriteLine(element);
+            }
+
+            foreach (var element in collections.Collections)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
