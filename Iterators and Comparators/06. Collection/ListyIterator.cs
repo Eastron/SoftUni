97040c3740f1ec1224875295f4ec1a731303a20c
Pascal Angelov <pascal_angelov@abv.bg>
2020-10-26 20:26:16 +0200

    Add files via upload

---
+++ Iterators and Comparators/06. Collection/ListyIterator.cs
@@ -0,0 +1,65 @@
+﻿namespace IteratorsAndComparators
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+   public class ListyIterator<T> : IEnumerable<T>
+    {
+        private int position = 0;
+        public int Position { get; set; }
+        public List<T> Collections { get; set; }
+
+        public ListyIterator()
+        {
+            this.Position = position;
+            this.Collections = new List<T>();
+        }
+
+        public bool Move(List<T> collections)
+        {
+            if (position + 1 < collections.Count )
+            {
+                position++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext(List<T> collections)
+        {
+            if (position + 1 == collections.Count)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Print(List<T> collections)
+        {
+            if (collections.Count == 0)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(collections[position]);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+
+            foreach (var item in this.Collections)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators/06. Collection/StartUp.cs
@@ -0,0 +1,60 @@
+﻿namespace IteratorsAndComparators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            ListyIterator<string> collections = new ListyIterator<string>();
+            while (input != "END")
+            {
+                string[] token = input.Split();
+                switch (token[0])
+                {
+                    case "Create":
+                        {
+                            if (token.Length > 0)
+                            {
+                                for (int i = 1; i < token.Length; i++)
+                                {
+                                    collections.Collections.Add(token[i]);
+                                }
+                            }   
+                        }
+                        break;
+                    case "Move":
+                        {
+                            Console.WriteLine(collections.Move(collections.Collections));
+                        }
+                        break;
+                    case "Print":
+                        {
+                           collections.Print(collections.Collections);
+                        }
+                        break;
+                    case "HasNext":
+                        {
+                            Console.WriteLine(collections.HasNext(collections.Collections));
+                        }
+                        break;
+                    case "PrintAll":
+                        {
+                            foreach (var item in collections.Collections)
+                            {
+                                Console.Write(item + " ");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
